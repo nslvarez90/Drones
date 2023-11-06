b68ae3fd5996cfd26fc4c1a0ab00b5913f82ba0a
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-11-06 09:08:34 -0500

    Add:(Add models Dron and Medicine)

---
+++ Models/Dron.cs
@@ -0,0 +1,18 @@
+﻿namespace Drones.Models
+{   
+    public class Dron
+    {
+        public int Id { get; set; }
+        public string serialNumber { get; set; }
+        public string model { get; set; }      
+        public float batteryCapacity { get; set; }
+        public string status { get; set; }
+        public DateTime createAt { get; set; }
+
+        public  int maxWeight { get; set; }
+
+        public List<Medicine> medicines { get; set; } = new List<Medicine>();
+
+
+    }
+}
---
+++ Models/Medicine.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drones.Models
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int weight { get; set; }
+        public string code { get; set; }
+        [NotMapped]
+        [FromForm]
+        public IFormFile imagen { get; set; }
+    }
+}
