c398a925a4b7d2da0af4595c43bdbaf67270d446
Nelson Sanchez <nsanchez@pyxelsolution.com>
2023-11-06 09:09:15 -0500

    Add;(Dron and medicine context)

---
+++ Models/DronContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Drones.Models
+{
+    public class DronContext : DbContext
+    {
+        public DronContext(DbContextOptions<DronContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Dron> DronItems { get; set; } = null!;
+    }
+}
---
+++ Models/MedicineContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Drones.Models
+{
+    public class MedicineContext : DbContext
+    {
+        public MedicineContext(DbContextOptions<MedicineContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Medicine> MedicineItems { get; set; } = null!;
+    }
+}
